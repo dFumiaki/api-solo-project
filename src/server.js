fca38e69fecaa9b0044e351c510950408ca36e7c
dFumiaki <fumiaki.andou.fb@nttdocomo.com>
2023-03-29 01:49:00 +0900

    fumiaki's solo api

--- src/server.js
+++ src/server.js
@@ -1,78 +1,35 @@
 const express = require("express");
 const path = require("path");
+const idController = require("./id/id.controller");
 
-const customerController = require("./customer/customer.controller");
-const productController = require("./product/product.controller");
-const orderController = require("./order/order.controller");
+const setupServer = () => {
+  const app = express();
+  app.use(express.json());
 
-const app = express();
+  const validateId = (req, res, next) => {
+    const id = parseInt(req.params.id);
+    if (id) {
+      next();
+    } else {
+      res
+        .status(400)
+        .send(
+          "Invalid id parameter. Expecting number, received " +
+            typeof req.params.id
+        );
+    }
+  };
 
-/*
-  This adds JSON parsing middleware for incoming request
-  body with a Content-Type header of 'application/json'.
-  You don't need to worry about JSON.parse or JSON.stringify
-  when this middleware is used.
-*/
-app.use(express.json());
+  // ID routes
+  app.get("/pokemons", idController.index);
+  app.get("/pokemons/view/:id", validateId, idController.view);
+  //app.get("/pokemons/edit/:id", validateId, idController.edit);
+  //app.get("/pokemons/new", idController.new);
+  app.post("/pokemons/save", idController.save);
+  app.patch("/pokemons/update/:id", idController.update);
+  app.delete("/pokemons/delete/:id", idController.delete);
 
-// For parsing form data (application/x-www-form-urlencoded)
-app.use(express.urlencoded({ extended: true }));
-
-// This configures templates for the frontend of the app.
-app.set("views", `${__dirname}/templates`);
-app.set("view engine", "ejs");
-
-/*
-  This allows us to serve static files (html, css, etc.) from
-  the public directory.
-*/
-app.use(express.static(path.join(__dirname, "public")));
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.render("pages/index");
-});
-
-const validateId = (req, res, next) => {
-  const id = parseInt(req.params.id);
-  if (id) {
-    next();
-  } else {
-    res
-      .status(400)
-      .send(
-        "Invalid id parameter. Expecting number, received " +
-          typeof req.params.id
-      );
-  }
+  return app;
 };
 
-// Customer routes
-app.get("/customers", customerController.index);
-app.get("/customers/view/:id", validateId, customerController.view);
-app.get("/customers/edit/:id", validateId, customerController.edit);
-app.get("/customers/new", customerController.new);
-app.post("/customers/save", customerController.save);
-
-// product routes
-app.get("/products", productController.index);
-app.get("/products/view/:id", validateId, productController.view);
-app.get("/products/edit/:id", validateId, productController.edit);
-app.get("/products/new", productController.new);
-app.post("/products/save", productController.save);
-
-// Order routes
-app.get("/orders", orderController.index);
-app.get("/orders/view/:id", validateId, orderController.view);
-app.get("/orders/edit/:id", validateId, orderController.edit);
-app.get("/orders/new", orderController.new);
-app.post("/orders/save", orderController.save);
-
-app.get("/error", (req, res) => {
-  res.render("pages/error");
-});
+module.exports = { setupServer };
